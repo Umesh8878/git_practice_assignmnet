1c5db224d2dec728974dcdb7806d369156de61c2
Umesh <yomyouronlinemart@gmail.com>
2022-11-29 23:30:07 +0530

    created prime and pelindrome

---
+++ pelindrome.js
@@ -0,0 +1,7 @@
+let num = 121;
+let n = 0 , x;
+for(let a=num; a>0; a=a){
+    x = a % 10;
+    a = a / 10;
+}
+console.log("yes");
---
+++ prime.js
@@ -0,0 +1,8 @@
+let num = 13;
+let count =0;
+for(let i=2; i<13; i++){
+    if(num%i==0){
+        count++;
+    }
+}
+console.log(prime);
