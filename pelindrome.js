c9e803fc1375b4e6006c36b64513e5cddb310cfe
sourav Anil <umeshgupta78361@gmail.com>
2022-11-30 12:46:42 +0530

    new code added

--- pelindrome.js
+++ pelindrome.js
@@ -3,5 +3,11 @@ let n = 0 , x;
 for(let a=num; a>0; a=a){
     x = a % 10;
     a = a / 10;
+    n = x + a * 10;
 }
-console.log("yes");
+if(num == n){
+	console.log("Pelindrome");
+}
+else{
+	console.log("Not Pelindrome");
+}
--- prime.js
+++ prime.js
@@ -1,8 +1,13 @@
 let num = 13;
 let count =0;
-for(let i=2; i<13; i++){
+for(let i=2; i<num; i++){
     if(num%i==0){
         count++;
     }
 }
-console.log(prime);
+if(count>0){
+	console.log("Prime");
+}
+else{
+	console.log("Not Prime");
+}
